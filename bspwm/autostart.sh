73635ef8234469e7a009b66254c89a73a7321faf
RxdCrxwn <mkhairuman@gmail.com>
2023-03-29 23:45:38 +0700

    I forgor

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -14,9 +14,9 @@ function run {
 #xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 #autorandr horizontal
 
-#$HOME/.config/polybar/launch.sh --material &
+$HOME/.config/polybar/launch.sh --forest &
 #$HOME/.config/plbr/serenade/launch.sh &
-$HOME/.config/plbr/fail/launch.sh &
+#$HOME/.config/plbr/fail/launch.sh &
 
 #change your keyboard if you need it
 #setxkbmap -layout be
@@ -53,8 +53,8 @@ run xfce4-power-manager &
 numlockx on &
 #blueberry-tray &
 picom --config $HOME/.config/bspwm/picom.conf &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-/usr/lib/xfce4/notifyd/xfce4-notifyd &
+#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+#/usr/lib/xfce4/notifyd/xfce4-notifyd &
 #run volumeicon &
 nitrogen --restore &
 autored &
